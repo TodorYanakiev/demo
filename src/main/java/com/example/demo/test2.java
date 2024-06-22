364b88888d2a673ff33aa10325a6deaa6c4abb5d
TodorYanakiev <todoryanakiev395@gmail.com>
2024-06-22 11:05:30 +0300

    a

--- src/main/java/com/example/demo/test2.java
+++ src/main/java/com/example/demo/test2.java
@@ -1,4 +1,10 @@
 package com.example.demo;
 
+import java.util.Random;
+
 public class test2 {
+    public static int returnNumber() {
+        Random random = new Random();
+        return random.nextInt();
+    }
 }
